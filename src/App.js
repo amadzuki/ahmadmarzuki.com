fcc5b701c1501410a331cbc3763fd4fde1a65f4a
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-05-02 00:47:40 +0700

    Implemented react router and xstyled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,34 @@
 import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+import Navigation from "./components/Navigation"
+
+import Home from "./pages/home"
+import About from "./pages/about"
+import Contact from "./pages/contact"
+import Blog from "./pages/blog"
 
 const App = () => {
-  return <div>Personal Website Project</div>
+  return (
+    <Router>
+      <div>
+        <Navigation></Navigation>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 export default App
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,38 @@
 import React from "react"
-
 import styled from "@xstyled/styled-components"
+import { Link } from "react-router-dom"
 
-// const NavigationWrapper = styled.div`
-
-// `
+const NavigationWrapper = styled.div`
+  display: flex;
+`
+const LinkButton = styled.box`
+  padding: 1.5em;
+  margin: 1.5em;
+`
 
 const Navigation = () => {
-  return <div></div>
+  return (
+    <NavigationWrapper>
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <LinkButton>HOME</LinkButton>
+      </Link>
+      <LinkButton>
+        <Link to="/about" style={{ textDecoration: "none" }}>
+          ABOUT
+        </Link>
+      </LinkButton>
+      <LinkButton>
+        <Link to="/contact" style={{ textDecoration: "none" }}>
+          CONTACT
+        </Link>
+      </LinkButton>
+      <LinkButton>
+        <Link to="/blog" style={{ textDecoration: "none" }}>
+          BLOG
+        </Link>
+      </LinkButton>
+    </NavigationWrapper>
+  )
 }
+
+export default Navigation
