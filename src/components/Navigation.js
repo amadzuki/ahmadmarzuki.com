ed0851eb3a5cebe50fa98a30741b20ce1c595891
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-05-02 00:04:50 +0700

    Initialize pages and Navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import styled from "@xstyled/styled-components"
+
+// const NavigationWrapper = styled.div`
+
+// `
+
+const Navigation = () => {
+  return <div></div>
+}
