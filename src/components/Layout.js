a6c87ecfae008f12114a4793164a5b6cd5f4de14
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-05-02 11:29:41 +0700

    Refactor hero components in layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,18 +2,29 @@ import React from "react"
 import styled from "@xstyled/styled-components"
 
 import Navigation from "../components/Navigation"
-import MainHeading from "../components/MainHeading"
 
 const Hero = styled.box`
   height: 100vh;
   background: no-repeat center/cover url("/images/HeroImage.jpg");
 `
+const HeroHeader = styled.header`
+  height: 100%;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const HeroHeading = styled.h1`
+  font-size: 3em;
+`
 
-const MainButton = styled.box`
+const HeroButton = styled.button`
   font-size: 1.2rem;
   margin: 1em;
   padding: 0.5em 1em;
   background-color: primaryBackground;
+  border: none;
   border-radius: 0.5em;
   color: black;
 `
@@ -23,9 +34,10 @@ const Layout = ({ children }) => {
     <div>
       <Hero>
         <Navigation></Navigation>
-        <MainHeading>
-          <MainButton>See My Portfolio</MainButton>
-        </MainHeading>
+        <HeroHeader>
+          <HeroHeading>Hello, I'm Marzuki</HeroHeading>
+          <HeroButton>See My Portfolio</HeroButton>
+        </HeroHeader>
       </Hero>
       {children}
     </div>
