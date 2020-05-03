b92adb472ee9a58317f7a18e33ae9847d1438562
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-05-03 12:42:10 +0700

    Implement hide navigation on scroll

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,14 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "@xstyled/styled-components"
 import { Link } from "react-router-dom"
 
-const NavigationWrapper = styled.div`
+const NavigationWrapper = styled.box`
   display: flex;
   position: fixed;
 `
 const LinkBox = styled.box`
   padding: 0.5em;
-  margin: 2em;
+  margin: 2em 1.5em;
   color: whitesmoke;
   &:hover {
     color: orange;
@@ -17,12 +17,19 @@ const LinkBox = styled.box`
 
 const IconBox = styled.img`
   max-width: 3em;
+  margin: 1em;
 `
 
 const Navigation = () => {
+  const [display, setDisplay] = useState("flex")
+  const [position, setPosition] = useState(0)
+  window.addEventListener("scroll", () => {
+    window.pageYOffset <= position ? setDisplay("flex") : setDisplay("none")
+    setPosition(window.pageYOffset)
+  })
   return (
-    <NavigationWrapper>
-      <IconBox src="/images/icon-amadzuki-new.svg"></IconBox>
+    <NavigationWrapper display={display}>
+      <IconBox src="/images/icon-amadzuki-new.svg" />
       <Link to="/" style={{ textDecoration: "none" }}>
         <LinkBox>HOME</LinkBox>
       </Link>
