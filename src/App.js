71aa280498bda497cf8f01d49ba5912f1b50ba26
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-09-23 20:18:11 +0700

    Add resume path

--- src/App.js
+++ src/App.js
@@ -1,31 +1,39 @@
-import React from "react"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom'
 
-import Home from "./pages/home"
-import About from "./pages/about"
-import Contact from "./pages/contact"
-import Blog from "./pages/blog"
-import BlogPost from "./pages/blogPost"
+import Home from './pages/home'
+import About from './pages/about'
+import Contact from './pages/contact'
+import Blog from './pages/blog'
+import BlogPost from './pages/blogPost'
 
 const App = () => {
   return (
     <Router>
       <div>
         <Switch>
-          <Route path="/" exact>
+          <Route path='/' exact>
             <Home />
           </Route>
-          <Route path="/about">
+          <Route path='/about'>
             <About />
           </Route>
-          <Route path="/contact">
+          <Route path='/contact'>
             <Contact />
           </Route>
-          <Route path="/blog" exact>
+          <Route path='/blog' exact>
             <Blog />
           </Route>
-          <Route path="/blog/:postTitle">
+          <Route path='/blog/:postTitle'>
             <BlogPost />
+          </Route>
+          <Route path='/resume'>
+            <Redirect to='/Resume_Ahmad_Marzuki_v3.pdf'></Redirect>
           </Route>
         </Switch>
       </div>
